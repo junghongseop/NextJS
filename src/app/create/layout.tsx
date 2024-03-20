8d648e2e48a5c956f5b83f30eff4348953538850
junghongseop <a01082372487@gmail.com>
2024-03-20 20:35:02 +0900

    add :: /create로 이동했을때 페이지

---
+++ src/app/create/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout(props:any) {
+    return (
+    <form>
+        <h2>Create</h2>
+        {props.children}
+    </form>
+    )
+}
---
+++ src/app/create/page.tsx
@@ -0,0 +1,7 @@
+export default function Create() {
+    return (
+        <>Create!!</>
+    )
+}
+
+// page.js -> layout.js 없으면 create폴더의 page.js->src/layout.js를 찾아감
