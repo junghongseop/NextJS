abe7443ea698ad6c422e32dc07c56cc0a23a4e3a
junghongseop <a01082372487@gmail.com>
2024-03-22 08:21:34 +0900

    add :: Link태그로 바꿔서 싱글 페이지로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,15 +14,15 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <h1><a href="/">WEB</a></h1>
+        <h1><Link href="/">WEB</Link></h1>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
+          <li><Link href="/read/1">html</Link></li>
+          <li><Link href="/read/2">css</Link></li>
         </ol>
         {children}
         <ul>
-          <li><a href="/create">Create</a></li>
-          <li><a href="/update/1">Update</a></li>
+          <li><Link href="/create">Create</Link></li>
+          <li><Link href="/update/1">Update</Link></li>
           <li><input type="button" value="delete" /></li>
         </ul>
       </body>
