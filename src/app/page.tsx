d2cc426a82c57bc18f2822293fab225c24519f44
junghongseop <a01082372487@gmail.com>
2024-03-22 08:27:32 +0900

    add :: public에 있는 이미 가져 오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <h2>Welcome</h2>
       Hello, Web!
+      <br></br>
+      <img src="/hello.png"></img>
     </>
   );
 }
