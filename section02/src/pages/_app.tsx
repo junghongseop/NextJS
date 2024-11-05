6f6257ab1457a85f48987708d5776ba76caa24c9
junghongseop <a01082372487@gmail.com>
2024-11-05 19:32:52 +0900

    feat: 프리페칭 관련 주석및 코드 추가

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // React의 App 컴포넌트와 같이 다른 컴포넌트의 부모 컴포넌트 역할을 함
 // _app.tsx은 루트 컴포넌트 역할을 함
@@ -14,12 +15,21 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  // test 페이지에서는 프리페칭이 이루어지지 않는 이유
+  // 다른 페이지는 Link로 연결되어 있어서 프리페칭이 가능하지만
+  // test는 프로그래메틱한 코드로 작성되어 있어 프리페칭이 안됨 => useEffect를 사용하여 /test에서 프리패칭이 가능하도록 함
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href="/">index</Link>
         &nbsp;
-        <Link href="/search">search</Link>
+        {/* Link 태그에서 prefetch를 false라고 하면 prefech가 이루어지지 않음 */}
+        <Link href="/search" prefetch={false}>search</Link>
         &nbsp;
         <Link href="/book/1">book/1</Link>
         <div>
