6bf17df8765a614a98828d465f740c36b297ee87
junghongseop <a01082372487@gmail.com>
2024-11-06 16:10:29 +0900

    fix: 검색했을때 리스트가 나오도록 수정

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,16 +1,20 @@
 import SearchLayout from "@/components/layouts/search-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mocks/books.json";
+import ListItem from "@/components/main/BookList/ListItem/ListItem";
 
 // navigation은 App Router에서 사용함.
 // Page Router에서 사용하면 호환성 문제가 생길 수 있어 권장하지 않음
 // import { useRouter } from "next/navigation";
 
 const SearchPage = () => {
-  const router = useRouter();
-  const { q } = router.query;
-
-  return <h1>search {q}</h1>;
+  return (
+    <div>
+      {books.map((books) => (
+        <ListItem key={books.id} {...books} />
+      ))}
+    </div>
+  );
 };
 
 export default SearchPage;
